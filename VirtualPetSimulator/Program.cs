e1de9883fa0d7d1d8c321b8f114ce138f702f4b8
vignesh-muralidharan <vigneshmuralidharan40@gmail.com>
2024-02-26 14:03:32 +0530

    Restarting Development

--- VirtualPetSimulator/Program.cs
+++ VirtualPetSimulator/Program.cs
@@ -1,1 +1,13 @@
 ﻿//C# Program to implement a Virtual Pet Simulator
+using System;
+
+namespace PetSim
+{
+    class Main
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.")
+        }
+    }
+}
